584227bedeff28747c8ce73c0920bdf8de310bff
Metalax <sampannapokharel@gmail.com>
2024-01-16 23:40:01 +0545

    UI/Nav/implemented navbar with initial design and linked routes

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -1,9 +1,66 @@
+import { NavLink } from "react-router-dom";
 import "./style.css";
 
 export const Navbar = () => {
     return (
         <div className="navbar">
-            <h1>Navigation</h1>
+            <img
+                className="navbar-logo"
+                src="images/clover.gifts.png"
+                alt="store logo"
+            />
+            <ul className="navbar-items">
+                <li>
+                    <NavLink
+                        to="/"
+                        className={({ isActive }) =>
+                            isActive ? "active" : "inactive"
+                        }
+                    >
+                        Home
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to="/services"
+                        className={({ isActive }) =>
+                            isActive ? "active" : "inactive"
+                        }
+                    >
+                        Services
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to="/gallery"
+                        className={({ isActive }) =>
+                            isActive ? "active" : "inactive"
+                        }
+                    >
+                        Gallery
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to="/contact"
+                        className={({ isActive }) =>
+                            isActive ? "active" : "inactive"
+                        }
+                    >
+                        Contact
+                    </NavLink>
+                </li>
+            </ul>
+            <NavLink
+                to="/login"
+                className={({ isActive }) => (isActive ? "" : "inactive")}
+            >
+                <img
+                    className="navbar-account-icon"
+                    src="images/user-icon.png"
+                    alt="store logo"
+                />
+            </NavLink>
         </div>
     );
 };
