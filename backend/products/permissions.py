99aaba1c806b35515e3c40a262fc28fe09bc0167
Supraav <nepaladhikari33@gmail.com>
2024-02-15 12:13:18 +0545

    seller product

--- backend/products/permissions.py
+++ backend/products/permissions.py
@@ -1,6 +1,7 @@
 from rest_framework import permissions
 
 class IsSeller(permissions.BasePermission):
+    message = 'User is not seller'
     def has_permission(self, request, view):
         
         # Check if the user is authenticated and is a seller
--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,9 +1,21 @@
 from rest_framework import serializers, fields
 from accounts.models import User
-from products.models import Products
+from products.models import Products,SellerProduct
 
 
 class GetAllProductsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Products
         fields = '__all__'
+
+class ProductBySellerSerializer(serializers.Serializer):
+    user_id = serializers.UUIDField()
+    product_ids = serializers.ListField(child=serializers.IntegerField())
+
+    def create(self, validated_data):
+        user_id = validated_data.pop('user_id')
+        product_ids = validated_data.pop('product_ids')
+        seller_products = []
+        for product_id in product_ids:
+            seller_products.append(SellerProduct.objects.create(user_id=user_id, product_id=product_id))
+        return seller_products
